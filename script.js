64d93046247cb87a8be1280d67769bbaaa01243e
Remo3311 <remo33553311@gmail.com>
2024-05-30 01:20:08 +0300

    sharehood

---
+++ script.js
@@ -0,0 +1,9 @@
+function joinExisting() {
+    alert("Joining an existing one...");
+    // Add functionality to join an existing group
+}
+
+function showForm() {
+    document.getElementById('main-container').classList.add('hidden');
+    document.getElementById('form-container').classList.remove('hidden');
+}
